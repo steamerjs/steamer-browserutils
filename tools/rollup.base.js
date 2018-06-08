e48452e26bef82941884887efa2e5163af5ee24a
heyli <heyli@tencent.com>
2018-06-08 19:54:51 +0800

    add babel and other rollup plugins

--- tools/rollup.base.js
+++ tools/rollup.base.js
@@ -1,7 +1,24 @@
 import pkg from '../package.json';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 export default [
+    // browser-friendly UMD build
+    {
+        input: 'src/main.js',
+        output: {
+            name: 'index.umd.js',
+            file: pkg.browser,
+            format: 'umd'
+        },
+        plugins: [
+            babel(),
+            resolve(), // so Rollup can find `ms`
+            commonjs() // so Rollup can convert `ms` to an ES module
+        ]
+    },
+
     // CommonJS (for Node) and ES module (for bundlers) build.
     // (We could have three entries in the configuration array
     // instead of two, but it's quicker to generate multiple
@@ -10,22 +27,10 @@ export default [
     // `file` and `format` for each target)
     {
         input: 'src/main.js',
+        external: ['ms'],
         output: [
-            {
-                name: 'index.cjs.js',
-                file: pkg.main,
-                format: 'cjs'
-            },
-            {
-                name: 'index.umd.js',
-                file: pkg.browser,
-                format: 'umd'
-            },
-            {
-                name: 'index.esm.js',
-                file: pkg.module,
-                format: 'es'
-            }
+            { file: pkg.main, format: 'cjs', name: 'index.cjs.js' },
+            { file: pkg.module, format: 'es', name: 'index.esm.js' }
         ],
         plugins: [
             babel()
